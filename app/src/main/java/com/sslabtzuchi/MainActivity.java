c64c0585a076496637028cea42c707414a33e0dd
chsh1019 <nickchen14163662@gmail.com>
2020-10-06 13:09:33 +0800

    navigation improve

--- app/src/main/java/com/sslabtzuchi/MainActivity.java
+++ app/src/main/java/com/sslabtzuchi/MainActivity.java
@@ -1,11 +1,16 @@
 package com.sslabtzuchi;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.robotemi.sdk.BatteryData;
 import com.robotemi.sdk.Robot;
 import com.robotemi.sdk.TtsRequest;
+import com.robotemi.sdk.listeners.OnBatteryStatusChangedListener;
 import com.robotemi.sdk.listeners.OnGoToLocationStatusChangedListener;
 import com.robotemi.sdk.listeners.OnLocationsUpdatedListener;
 import com.robotemi.sdk.navigation.listener.OnDistanceToLocationChangedListener;
+import com.robotemi.sdk.navigation.model.SafetyLevel;
+import com.robotemi.*;
 
 
 
@@ -17,13 +22,14 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity implements OnLocationsUpdatedListener, OnGoToLocationStatusChangedListener, OnDistanceToLocationChangedListener {
-    private Button B1,B2;
+public class MainActivity extends AppCompatActivity implements OnLocationsUpdatedListener, OnBatteryStatusChangedListener, OnGoToLocationStatusChangedListener, OnDistanceToLocationChangedListener {
+    private Button B1,B2,B3;
     private Robot robot;
     private List<String> test1;
 
@@ -40,16 +46,21 @@ public class MainActivity extends AppCompatActivity implements OnLocationsUpdate
         Btmp.setAlpha(0.6f);
         Btmp = findViewById(R.id.b2);
         Btmp.setAlpha(0.6f);
+        Btmp = findViewById(R.id.b3);
+        Btmp.setAlpha(0.6f);
         robot = Robot.getInstance();
         robot.addOnLocationsUpdatedListener(this);
         robot.addOnGoToLocationStatusChangedListener(this);
+        robot.addOnBatteryStatusChangedListener(this);
+        Log.d("safe",robot.getNavigationSafety().getValue());
+        robot.setNavigationSafety(SafetyLevel.MEDIUM);
+        Log.d("safe2",robot.getNavigationSafety().getValue());
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         test1 = robot.getLocations();
-
         B1 = (Button) findViewById(R.id.b1);
         B1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,11 +82,32 @@ public class MainActivity extends AppCompatActivity implements OnLocationsUpdate
         B2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+             //   Log.d("test",Integer.toString(robot.getLocations().size()));//2
                 robot.goTo("302");
-
             }
         });
+        B3 = (Button) findViewById(R.id.b3);
+        B3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //   Log.d("test",Integer.toString(robot.getLocations().size()));//2
+                robot.goTo("elevator");
+
+//                Log.d("batt",Integer.toString((robot.getBatteryData().component1())));
+//               SafetyLevel safe =  robot.getNavigationSafety().getValue();
+//                Log.d("safe",robot.getNavigationSafety().getValue());
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        robot.removeOnLocationsUpdateListener(this);
+        robot.removeOnGoToLocationStatusChangedListener(this);
+        robot.removeOnDistanceToLocationChangedListener(this);
+        robot.removeOnBatteryStatusChangedListener(this);
+
     }
 
     @Override
@@ -84,8 +116,8 @@ public class MainActivity extends AppCompatActivity implements OnLocationsUpdate
     }
     @Override
     public void onGoToLocationStatusChanged(String s, String s1, int i, String s2) {
-        Log.d("s1",s1);
-        Log.d("s2",s2);
+        Log.v("s1",s1);
+        Log.v("s2",s2);
         if (s1.equals("complete"))
         {
 //            TtsRequest temp = TtsRequest.create("中文測試",true);
@@ -97,5 +129,14 @@ public class MainActivity extends AppCompatActivity implements OnLocationsUpdate
     @Override
     public void onDistanceToLocationChanged(@NotNull Map<String, Float> map) {
 
+    }
+
+    @Override
+    public void onBatteryStatusChanged(@Nullable BatteryData batteryData) {
+//        Log.d("test","change");
+//        if (robot.getBatteryData().component1()<=85) {
+//            robot.goTo("home base");
+//            Log.d("test","change92");
+//        }
     }
 }
