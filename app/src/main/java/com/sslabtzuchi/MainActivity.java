ffd258adf5803fcabeb15e6029d7c00e4f26caf2
chsh1019 <nickchen14163662@gmail.com>
2020-10-07 16:57:15 +0800

    Tts speak

--- app/src/main/java/com/sslabtzuchi/MainActivity.java
+++ app/src/main/java/com/sslabtzuchi/MainActivity.java
@@ -11,7 +11,8 @@ import com.robotemi.sdk.listeners.OnLocationsUpdatedListener;
 import com.robotemi.sdk.navigation.listener.OnDistanceToLocationChangedListener;
 import com.robotemi.sdk.navigation.model.SafetyLevel;
 import com.robotemi.*;
-
+import com.robotemi.sdk.permission.OnRequestPermissionResultListener;
+import com.robotemi.sdk.permission.Permission;
 
 
 import android.content.Intent;
@@ -24,12 +25,14 @@ import android.widget.ImageView;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements OnLocationsUpdatedListener, OnBatteryStatusChangedListener, OnGoToLocationStatusChangedListener, OnDistanceToLocationChangedListener {
-    private Button B1,B2,B3;
+public class MainActivity extends AppCompatActivity implements OnRequestPermissionResultListener, OnLocationsUpdatedListener, OnBatteryStatusChangedListener, OnGoToLocationStatusChangedListener, OnDistanceToLocationChangedListener {
+    private Button B1,B2,B3,B4;
+    TtsRequest goat = TtsRequest.create("前往目的地",true);
     private Robot robot;
     private List<String> test1;
 
@@ -52,15 +55,17 @@ public class MainActivity extends AppCompatActivity implements OnLocationsUpdate
         robot.addOnLocationsUpdatedListener(this);
         robot.addOnGoToLocationStatusChangedListener(this);
         robot.addOnBatteryStatusChangedListener(this);
+        robot.addOnRequestPermissionResultListener(this);
+        robot.requestToBeKioskApp();
+//        Permission setting = Permission.SETTINGS;
+//        List <Permission> Authority = new ArrayList<>();
+//        Authority.add(setting);
+//        robot.requestPermissions(Authority,1);
+//        Log.d("setting",Integer.toString(robot.checkSelfPermission(Permission.SETTINGS)));
+//        Log.d("kiosk",Boolean.toString(robot.isSelectedKioskApp()));
         Log.d("safe",robot.getNavigationSafety().getValue());
         robot.setNavigationSafety(SafetyLevel.MEDIUM);
         Log.d("safe2",robot.getNavigationSafety().getValue());
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        test1 = robot.getLocations();
         B1 = (Button) findViewById(R.id.b1);
         B1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,11 +83,54 @@ public class MainActivity extends AppCompatActivity implements OnLocationsUpdate
 
             }
         });
+
+        Toast.makeText(this, robot.getNavigationSafety().getValue(), Toast.LENGTH_LONG).show();
+        Toast.makeText(this, Integer.toString(robot.checkSelfPermission(Permission.SETTINGS)), Toast.LENGTH_LONG).show();
+        Toast.makeText(this, Boolean.toString(robot.isSelectedKioskApp()), Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        test1 = robot.getLocations();
+        B4 = (Button) findViewById(R.id.b4);
+        B4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+
+            }
+        });
+        B1 = (Button) findViewById(R.id.b1);
+        B1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                robot.speak(goat);
+                try{
+                    // delay 0.8 second
+                    Thread.sleep(800);
+
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+//                test1 = robot.getLocations();
+//                Log.d("test2",test1.toString());
+//                Log.d("test3",test1.get(0).toString());
+//                robot.goTo(test1.get(0).toString());
+//               B1.setText("ttt");
+                robot.goTo("home base");
+
+            }
+        });
         B2 = (Button) findViewById(R.id.b2);
         B2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
              //   Log.d("test",Integer.toString(robot.getLocations().size()));//2
+                robot.speak(goat);
                 robot.goTo("302");
             }
         });
@@ -90,9 +138,9 @@ public class MainActivity extends AppCompatActivity implements OnLocationsUpdate
         B3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //   Log.d("test",Integer.toString(robot.getLocations().size()));//2
-                robot.goTo("elevator");
+                robot.speak(goat);
 
+                robot.goTo("elevator");
 //                Log.d("batt",Integer.toString((robot.getBatteryData().component1())));
 //               SafetyLevel safe =  robot.getNavigationSafety().getValue();
 //                Log.d("safe",robot.getNavigationSafety().getValue());
@@ -107,6 +155,7 @@ public class MainActivity extends AppCompatActivity implements OnLocationsUpdate
         robot.removeOnGoToLocationStatusChangedListener(this);
         robot.removeOnDistanceToLocationChangedListener(this);
         robot.removeOnBatteryStatusChangedListener(this);
+        robot.removeOnRequestPermissionResultListener(this);
 
     }
 
@@ -116,13 +165,16 @@ public class MainActivity extends AppCompatActivity implements OnLocationsUpdate
     }
     @Override
     public void onGoToLocationStatusChanged(String s, String s1, int i, String s2) {
-        Log.v("s1",s1);
-        Log.v("s2",s2);
+        TtsRequest temp;
         if (s1.equals("complete"))
         {
-//            TtsRequest temp = TtsRequest.create("中文測試",true);
-//            robot.speak(temp);
-            Log.d("test","finish");
+            temp = TtsRequest.create("到達目的地",true);
+            robot.speak(temp);
+        }
+        else if (s1.equals("going"))
+        {
+            temp = TtsRequest.create("機器人移動中，請小心",true);
+            robot.speak(temp);
         }
     }
 
@@ -138,5 +190,14 @@ public class MainActivity extends AppCompatActivity implements OnLocationsUpdate
 //            robot.goTo("home base");
 //            Log.d("test","change92");
 //        }
+    }
+
+    @Override
+    public void onRequestPermissionResult(@NotNull Permission permission, int i, int i1) {
+        Log.d("setting-r",Integer.toString(robot.checkSelfPermission(Permission.SETTINGS)));
+        Log.d("kiosk-r",Boolean.toString(robot.isSelectedKioskApp()));
+        Log.d("safe-r",robot.getNavigationSafety().getValue());
+        robot.setNavigationSafety(SafetyLevel.MEDIUM);
+        Log.d("safe2-r",robot.getNavigationSafety().getValue());
     }
 }
