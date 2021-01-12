a44eb9cbc12d22047bb626883f9dd2ca6867172c
chsh1019 <nickchen14163662@gmail.com>
2021-01-12 14:44:14 +0800

    INDONESIAN translate

--- app/src/main/java/com/sslabtzuchi/Languagetranslate.java
+++ app/src/main/java/com/sslabtzuchi/Languagetranslate.java
@@ -26,7 +26,102 @@ import java.io.IOException;
 import taobe.tec.jcc.JChineseConvertor;
 
 public class Languagetranslate extends AppCompatActivity {
-    Button home,tran,tran2,tts,tts2;
+    Button home,tran,tran2,tts,tts2,tran3,tran4;
+    //印尼翻中文
+    void INtoZH (String speechtext)
+    {
+        TranslatorOptions options =
+                new TranslatorOptions.Builder()
+                        .setSourceLanguage(TranslateLanguage.INDONESIAN)
+                        .setTargetLanguage(TranslateLanguage.CHINESE)
+                        .build();
+        final Translator englishChineseTranslator =
+                Translation.getClient(options);
+        DownloadConditions conditions = new DownloadConditions.Builder()
+                .requireWifi()
+                .build();
+        englishChineseTranslator.downloadModelIfNeeded(conditions);
+        englishChineseTranslator.translate(speechtext)
+                .addOnSuccessListener(
+                        new OnSuccessListener<String>() {
+                            @Override
+                            public void onSuccess(@NonNull String translatedText) {
+                                // Translation successful.
+                                Log.d("stt123",translatedText);
+                                try {
+                                    TextView result = (TextView) findViewById(R.id.LanTranslate2);
+                                    JChineseConvertor jChineseConvertor = JChineseConvertor
+                                            .getInstance();
+                                    translatedText = jChineseConvertor.s2t(translatedText);
+                                    Log.d("stt456",translatedText);
+//                                    TextView result1 = (TextView)findViewById(R.id.tranlatetext);
+//                                    result1.setText(translatedText);
+                                    result.setText(translatedText);
+                                    //  robot.speak(TtsRequest.create(translatedText,false));
+
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Error.
+                                // ...
+                            }
+                        });
+    }
+    //中文翻印尼
+    void ZHtoIN (String speechtext)
+    {
+        TranslatorOptions options =
+                new TranslatorOptions.Builder()
+                        .setSourceLanguage(TranslateLanguage.CHINESE)
+                        .setTargetLanguage(TranslateLanguage.INDONESIAN)
+                        .build();
+        final Translator englishChineseTranslator =
+                Translation.getClient(options);
+        DownloadConditions conditions = new DownloadConditions.Builder()
+                .requireWifi()
+                .build();
+        englishChineseTranslator.downloadModelIfNeeded(conditions);
+        englishChineseTranslator.translate(speechtext)
+                .addOnSuccessListener(
+                        new OnSuccessListener<String>() {
+                            @Override
+                            public void onSuccess(@NonNull String translatedText) {
+                                // Translation successful.
+                                Log.d("stt123",translatedText);
+                                try {
+                                    TextView result = (TextView) findViewById(R.id.LanTranslate2);
+                                    JChineseConvertor jChineseConvertor = JChineseConvertor
+                                            .getInstance();
+                                    translatedText = jChineseConvertor.s2t(translatedText);
+                                    Log.d("stt456",translatedText);
+//                                    TextView result1 = (TextView)findViewById(R.id.tranlatetext);
+//                                    result1.setText(translatedText);
+                                    result.setText(translatedText);
+                                    //  robot.speak(TtsRequest.create(translatedText,false));
+
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Error.
+                                // ...
+                            }
+                        });
+    }
+
     //英翻中
     void ENtoZH(String speechtext)
     {
@@ -124,6 +219,8 @@ public class Languagetranslate extends AppCompatActivity {
         home = (Button) findViewById(R.id.home);
         tran = (Button) findViewById(R.id.tran);
         tran2 = (Button) findViewById(R.id.tran2);
+        tran3 = (Button) findViewById(R.id.tran3);
+        tran4 = (Button) findViewById(R.id.tran4);
         tts = (Button) findViewById(R.id.tts);
         tts2 = (Button) findViewById(R.id.tts2);
         //回主頁面
@@ -151,6 +248,23 @@ public class Languagetranslate extends AppCompatActivity {
                 ENtoZH(data.getText().toString());
             }
         });
+        //中翻印尼
+        tran3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText data = (EditText) findViewById(R.id.LanTranslate1);
+                ZHtoIN(data.getText().toString());
+            }
+        });
+        //印尼翻中文
+        tran4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText data = (EditText) findViewById(R.id.LanTranslate1);
+                INtoZH(data.getText().toString());
+            }
+        });
+
         //
         tts.setOnClickListener(new View.OnClickListener() {
             @Override
